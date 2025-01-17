51ce6456ddb671f6801f55490dfecacdaa276651
jhe <jhe@brobridge.com>
2025-01-17 11:55:17 +0800

    added scratch and update workflow

--- build/docker/generate-key.sh
+++ build/docker/generate-key.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-export NKEYS_PATH=/datastore/.nsc/nkeys
-export XDG_DATA_HOME=/datastore/.nsc
-export XDG_CONFIG_HOME=/datastore/.nsc/.config
-
-nsc update
+#export NKEYS_PATH=/datastore/.nsc/nkeys
+#export XDG_DATA_HOME=/datastore/.nsc
+#export XDG_CONFIG_HOME=/datastore/.nsc/.config
+#
+#nsc update --config-dir=/datastore/.nsc/.config --data-dir=/datastore/.nsc --keystore-dir=/datastore/.nsc/nkey
 
 nsc init -d /datastore/.nsc/nats -n jetOperator
 nsc edit operator --service-url nats://localhost:4222
